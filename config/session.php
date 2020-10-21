0108f618a54dff6b031e97f7347bf9a9ce330ec9
xeferino <josegregoriolozadae@gmail.com>
2020-10-21 12:37:11 -0400

    session infinity one year

--- config/session.php
+++ config/session.php
@@ -31,8 +31,8 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
-
+    #'lifetime' => env('SESSION_LIFETIME', 120),
+    'lifetime' => env('SESSION_LIFETIME', 1 * (60 * 24 * 365)),
     'expire_on_close' => false,
 
     /*
